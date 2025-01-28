1005c09e2d06d99a15def57b230fe49711dcd436
Aparna Paul <aparnapaul55@gmail.com>
2025-01-28 19:37:56 +1030

    displaying products statically

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cards from './components/Cards'
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Cards />
+    
+    </>
+  )
+}
+
+export default App
